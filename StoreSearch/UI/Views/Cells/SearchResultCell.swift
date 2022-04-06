99d45c8f954936485b7b841f118e03f62a88aee9
Jordan Harvey-Morgan <jharveymorgan11@gmail.com>
2022-04-05 21:21:07 -0400

    download images

--- StoreSearch/UI/Views/Cells/SearchResultCell.swift
+++ StoreSearch/UI/Views/Cells/SearchResultCell.swift
@@ -14,6 +14,10 @@ class SearchResultCell: UITableViewCell {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var artistNameLabel: UILabel!
     @IBOutlet weak var artworkImageView: UIImageView!
+    
+    var downloadTask: URLSessionDownloadTask?
+    
+    // MARK: - Lifecycle
 
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -23,6 +27,13 @@ class SearchResultCell: UITableViewCell {
         
         selectedBackgroundView = selectedView
     }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        downloadTask?.cancel()
+        downloadTask = nil
+    }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
@@ -30,4 +41,24 @@ class SearchResultCell: UITableViewCell {
         // Configure the view for the selected state
     }
 
+}
+
+// MARK: - Helper Methods
+
+extension SearchResultCell {
+    
+    func configure(for result: SearchResult) {
+        nameLabel.text = result.name
+        
+        if result.artist.isEmpty {
+            artistNameLabel.text = "Unknown"
+        } else {
+            artistNameLabel.text = "\(result.artist) (\(result.type))"
+        }
+        
+        artworkImageView.image = UIImage(systemName: "square")
+        if let smallURL = URL(string: result.imageSmall) {
+            downloadTask = artworkImageView.loadImage(url: smallURL)
+        }
+    }
 }
